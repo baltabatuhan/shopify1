cedea448ed6562439e5d9c2364c4a9e8df4508d8
Batuhan Balta <bthn.balta@gmail.com>
2021-09-20 00:50:17 -0700

    skeleteon created

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from "antd";
+
+const { Header, Content, Footer } = Layout
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Header>Header</Header>
+      <Content className="site-layout" style={{ padding: "0 50px", marginTop: 64 }}>Content</Content>
+      <Footer style={{ textAlign: 'center' }}>Designed by Batuhan Balta</Footer>
+    </Layout>
+  )
 }
 
 export default App;
